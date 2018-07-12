8aa7869eaae6c7604375ee94ae70178389f26a47
root <root@su18-cs411-12.cs.illinois.edu>
2018-07-11 20:23:31 -0500

    add search function

--- flaskdemo.py
+++ flaskdemo.py
@@ -23,11 +23,23 @@ def index():
 @app.route('/showmovies', methods = ['GET'])
 def showmovies():
 	with app.app_context():
+		moviesearch = request.args.get('movie')
 		cur = mysql.connection.cursor()
-		cur.execute("select title, full_name as director, rating from movies, directors, ratings, names where movies.movie_id = directors.movie_id AND DIRECTOR = name_id AND movies.movie_id = ratings.movie_id;")
+		cur.execute(("select title, movie_id, full_name as director, rating from movies, directors, ratings, names where movies.movie_id = directors.movie_id AND DIRECTOR = name_id AND movies.movie_id = ratings.movie_id AND title LIKE '%{}%';").format(moviesearch))
 		rows = cur.fetchall()
 		cur.close()
 		return render_template('showmovies.html', data=rows)
+
+def addfavorites():
+	with app.app_context():
+		moviesearch = request.args.get('favmovie')
+		cur = mysql.connection.cursor()
+		cur.execute(("INSERT INTO favorites (user_id, movie_id) VALUES ('aa1234', '{}';).format(moviesearch))
+		cur.commit()
+		cur.execute(("select title from movies, favorites where movies.movie_id = favorites.movie_id AND favorites.user_id = 'aa1234'))
+		rows = cur.fetchall()
+		cur.close()
+		return render_template('shofavorites.html', data=rows)
 
 
 if __name__ == "__main__":
