a179c12dd33249062c2938983f1fa126dff1ed38
szhao1968 <38966409+szhao1968@users.noreply.github.com>
2018-07-14 16:16:56 -0500

    Update flaskdemo.py

--- flaskdemo.py
+++ flaskdemo.py
@@ -15,20 +15,23 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return index()
+
 
 
 @app.route("/")
 def index():
-    return render_template('index2.html')
+    if not session.get('logged_in'):
+	return render_template('login.html')
+    else:
+    	return render_template('index2.html')
 
 @app.route('/showmovies', methods = ['GET'])
 def showmovies():
