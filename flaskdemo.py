61fa5a96c1238ee1a929620ac5f90af1dfa29cd2
szhao1968 <38966409+szhao1968@users.noreply.github.com>
2018-07-25 16:34:18 -0500

    Update flaskdemo.py

--- flaskdemo.py
+++ flaskdemo.py
@@ -3,30 +3,49 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
 import os
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+ 
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = 'username']
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'movies'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
-
-
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello!  <a href='/logout'>Logout</a>"
+ 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+ 
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
         session['logged_in'] = True
     else:
         flash('wrong password!')
-    return index()
-
+    return home()
+ 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return index()
+    return home()
+
 
 @app.route("/")
 def index():
